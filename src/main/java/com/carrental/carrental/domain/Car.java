e0b4748d82aabb251893fad43c7adf71c853f9aa
Ariel Silva <nb27588@novabase.pt>
2021-12-17 09:02:19 +0000

    Begin Car domain object

---
+++ src/main/java/com/carrental/carrental/domain/Car.java
@@ -0,0 +1,20 @@
+package com.carrental.carrental.domain;
+
+import java.util.Date;
+
+public class Car {
+    private String licensePlate;
+    private brand;
+    private photos;
+    private fee;
+    private fuelType;
+    private String model;
+    private Date registration_dat;
+    private int seats;
+    private size;
+    private transmission;
+    private float weight;
+    private Date manufactureYear;
+
+}
+
