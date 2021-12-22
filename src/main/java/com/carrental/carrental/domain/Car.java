72181e9f7bfe940ce5c511c733b884a050b767ec
Ariel Silva <nb27588@novabase.pt>
2021-12-22 08:15:33 +0000

    Change Car getters and setters

--- src/main/java/com/carrental/carrental/domain/Car.java
+++ src/main/java/com/carrental/carrental/domain/Car.java
@@ -1,27 +1,39 @@
 package com.carrental.carrental.domain;
 
 import com.carrental.carrental.domain.enums.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.awt.image.BufferedImage;
 import java.util.Date;
 
-@Getter
-@Setter
+@AllArgsConstructor
 public class Car {
 
+    @Getter
     private String licensePlate;
+    @Getter
     private Brand brand;
+    @Getter
     private String model;
+    @Getter
     private Date registrationDate;
+    @Getter
     private Transmission transmission;
+    @Getter
     private float weight;
+    @Getter
     private Date manufactureYear;
+    @Getter
     private int seats;
+    @Getter @Setter
     private Fuel fuelType;
+    @Getter @Setter
     private Fee fee;
+    @Getter @Setter
     private BufferedImage photos;
+    @Getter
     private CarSize size;
 }
 
