b4a8ac8bbe4618759343c9b728db990fb4e27f96
Ariel Silva <nb27588@novabase.pt>
2021-12-27 08:07:48 +0000

    Improve Car and Rent domain

--- src/main/java/com/carrental/carrental/domain/Car.java
+++ src/main/java/com/carrental/carrental/domain/Car.java
@@ -11,13 +11,15 @@ import java.util.Date;
 
 public class Car {
 
-    public Car(String licensePlate, BrandEnum brand, String model, LocalDate registrationDate, TransmissionEnum transmission, float weight, LocalDate manufactureYear, int seats, FuelEnum fuelType, int fee, CarSizeEnum size) {
+    public Car(String licensePlate, BrandEnum brand, String model, LocalDate registrationDate, int mileage, TransmissionEnum transmission, float weight, ColorEnum color,LocalDate manufactureYear, int seats, FuelEnum fuelType, int fee, CarSizeEnum size) {
         this.licensePlate = licensePlate;
         this.brand = brand;
         this.model = model;
         this.registrationDate = registrationDate;
+        this.mileage = mileage;
         this.transmission = transmission;
         this.weight = weight;
+        this.color = color;
         this.manufactureYear = manufactureYear;
         this.seats = seats;
         this.fuelType = fuelType;
@@ -34,9 +36,13 @@ public class Car {
     @Getter
     private LocalDate registrationDate;
     @Getter
+    private int mileage;
+    @Getter
     private TransmissionEnum transmission;
     @Getter
     private float weight;
+    @Getter
+    private ColorEnum color;
     @Getter
     private LocalDate manufactureYear;
     @Getter
