cde559625f25d2bee6a348cbaf059c5aa4f5f348
Ariel Silva <ariel120292@gmail.com>
2022-03-13 10:28:38 +0000

    Add Lombok annotations to CreditCard

--- src/main/java/com/carrental/carrental/domain/CreditCard.java
+++ src/main/java/com/carrental/carrental/domain/CreditCard.java
@@ -1,37 +1,18 @@
 package com.carrental.carrental.domain;
 
 import com.carrental.carrental.domain.enums.CreditCardTypeEnum;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@Getter
 public class CreditCard {
 
     private String number;
     private User cardHolder;
     private LocalDate expirationDate;
     private CreditCardTypeEnum cardType;
-
-    public CreditCard(String number, User cardHolder, LocalDate expirationDate, CreditCardTypeEnum cardType) {
-        this.number = number;
-        this.cardHolder = cardHolder;
-        this.expirationDate = expirationDate;
-        this.cardType = cardType;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public User getCardHolder() {
-        return cardHolder;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public CreditCardTypeEnum getCardType() {
-        return cardType;
-    }
 
 }
