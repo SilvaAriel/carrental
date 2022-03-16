1f52df1e55fea594b18f5767951532d171cdfaa8
Ariel Silva <ariel120292@gmail.com>
2022-03-16 17:03:56 +0000

    Add NonNull to Rent

--- src/test/java/com/carrental/carrental/domain/RentTest.java
+++ src/test/java/com/carrental/carrental/domain/RentTest.java
@@ -26,7 +26,7 @@ public class RentTest {
                 "Doe", new Role("renter"), "ABC",now, "Some Street",
                 "1", "Cool", "City", CountryCode.CO, "123456");
         this.carOne = new Car("ABC1234", new Brand("Chevrolet"), "Corsa",
-                LocalDate.now(), 0, TransmissionEnum.A, 100, new Color("black"),
+                LocalDate.now(), 0, TransmissionEnum.A, new Color("black"),
                 LocalDate.now(), 4, FuelEnum.ETHANOL, 100, CarSizeEnum.ECONOMY_CAR);
         this.carList = Arrays.asList(this.carOne);
         this.rentOne = new Rent(now, now.plusDays(5), 150, carList, null, userOne, 1000, false, 0);
